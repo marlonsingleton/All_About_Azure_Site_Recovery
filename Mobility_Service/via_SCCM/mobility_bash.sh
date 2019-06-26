a70a84d49addfbc19112d089200d67a26a155519
Marlon Singleton <marlonosingleton@yahoo.com>
2019-06-26 12:10:24 +0300

    created breakdown bash file

---
+++ Mobility_Service/via_SCCM/mobility_bash.sh
@@ -0,0 +1,133 @@
+#!/usr/bin/env bash
+
+# Code by Microsoft
+# Breakdown by Marlon Singleton
+
+rm -rf /tmp/MobSvc
+mkdir -p /tmp/MobSvc
+INSTALL_DIR='/usr/local/ASR'
+VX_VERSION_FILE='/usr/local/.vx_version'
+
+echo "=============================" >> /tmp/MobSvc/sccm.log
+echo `date` >> /tmp/MobSvc/sccm.log
+echo "=============================" >> /tmp/MobSvc/sccm.log
+
+if [ -f /etc/oracle-release ] && [ -f /etc/redhat-release ]; then
+    if grep -q 'Oracle Linux Server release 6.*' /etc/oracle-release; then
+        if uname -a | grep -q x86_64; then
+            OS="OL6-64"
+            echo $OS >> /tmp/MobSvc/sccm.log
+            cp *OL6*.tar.gz /tmp/MobSvc
+        fi
+    fi
+elif [ -f /etc/redhat-release ]; then
+    if grep -q 'Red Hat Enterprise Linux Server release 6.* (Santiago)' /etc/redhat-release || \
+        grep -q 'CentOS Linux release 6.* (Final)' /etc/redhat-release || \
+        grep -q 'CentOS release 6.* (Final)' /etc/redhat-release; then
+        if uname -a | grep -q x86_64; then
+            OS="RHEL6-64"
+            echo $OS >> /tmp/MobSvc/sccm.log
+            cp *RHEL6*.tar.gz /tmp/MobSvc
+        fi
+    elif grep -q 'Red Hat Enterprise Linux Server release 7.* (Maipo)' /etc/redhat-release || \
+        grep -q 'CentOS Linux release 7.* (Core)' /etc/redhat-release; then
+        if uname -a | grep -q x86_64; then
+            OS="RHEL7-64"
+            echo $OS >> /tmp/MobSvc/sccm.log
+            cp *RHEL7*.tar.gz /tmp/MobSvc
+                fi
+    fi
+elif [ -f /etc/SuSE-release ] && grep -q 'VERSION = 11' /etc/SuSE-release; then
+    if grep -q "SUSE Linux Enterprise Server 11" /etc/SuSE-release && grep -q 'PATCHLEVEL = 3' /etc/SuSE-release; then
+        if uname -a | grep -q x86_64; then
+            OS="SLES11-SP3-64"
+            echo $OS >> /tmp/MobSvc/sccm.log
+            cp *SLES11-SP3*.tar.gz /tmp/MobSvc
+        fi
+    elif grep -q "SUSE Linux Enterprise Server 11" /etc/SuSE-release && grep -q 'PATCHLEVEL = 4' /etc/SuSE-release; then
+        if uname -a | grep -q x86_64; then
+            OS="SLES11-SP4-64"
+            echo $OS >> /tmp/MobSvc/sccm.log
+            cp *SLES11-SP4*.tar.gz /tmp/MobSvc
+        fi
+    fi
+elif [ -f /etc/lsb-release ] ; then
+    if grep -q 'DISTRIB_RELEASE=14.04' /etc/lsb-release ; then
+       if uname -a | grep -q x86_64; then
+           OS="UBUNTU-14.04-64"
+           echo $OS >> /tmp/MobSvc/sccm.log
+           cp *UBUNTU-14*.tar.gz /tmp/MobSvc
+       fi
+    fi
+else
+    exit 1
+fi
+
+if [ -z "$OS" ]; then
+    exit 1
+fi
+
+Install()
+{
+    echo "Perform Installation." >> /tmp/MobSvc/sccm.log
+    ./install -q -d ${INSTALL_DIR} -r MS -v VmWare
+    RET_VAL=$?
+    echo "Installation Returncode: $RET_VAL" >> /tmp/MobSvc/sccm.log
+    if [ $RET_VAL -eq 0 ]; then
+        echo "Installation has succeeded. Proceed to configuration." >> /tmp/MobSvc/sccm.log
+        Configure
+    else
+        echo "Installation has failed." >> /tmp/MobSvc/sccm.log
+        exit $RET_VAL
+    fi
+}
+
+Configure()
+{
+    echo "Perform configuration." >> /tmp/MobSvc/sccm.log
+    ${INSTALL_DIR}/Vx/bin/UnifiedAgentConfigurator.sh -i [CSIP] -P MobSvc.passphrase
+    RET_VAL=$?
+    echo "Configuration Returncode: $RET_VAL" >> /tmp/MobSvc/sccm.log
+    if [ $RET_VAL -eq 0 ]; then
+        echo "Configuration has succeeded." >> /tmp/MobSvc/sccm.log
+    else
+        echo "Configuration has failed." >> /tmp/MobSvc/sccm.log
+        exit $RET_VAL
+    fi
+}
+
+Upgrade()
+{
+    echo "Perform Upgrade." >> /tmp/MobSvc/sccm.log
+    ./install -q -v VmWare
+    RET_VAL=$?
+    echo "Upgrade Returncode: $RET_VAL" >> /tmp/MobSvc/sccm.log
+    if [ $RET_VAL -eq 0 ]; then
+        echo "Upgrade has succeeded." >> /tmp/MobSvc/sccm.log
+    else
+        echo "Upgrade has failed." >> /tmp/MobSvc/sccm.log
+        exit $RET_VAL
+    fi
+}
+
+cp MobSvc.passphrase /tmp/MobSvc
+cd /tmp/MobSvc
+
+tar -zxvf *.tar.gz
+
+if [ -e ${VX_VERSION_FILE} ]; then
+    echo "${VX_VERSION_FILE} exists. Checking for configuration status." >> /tmp/MobSvc/sccm.log
+    agent_configuration=$(grep ^AGENT_CONFIGURATION_STATUS "${VX_VERSION_FILE}" | cut -d"=" -f2 | tr -d " ")
+    echo "agent_configuration=$agent_configuration" >> /tmp/MobSvc/sccm.log
+     if [ "$agent_configuration" == "Succeeded" ]; then
+        echo "Agent is already configured. Proceed to Upgrade." >> /tmp/MobSvc/sccm.log
+        Upgrade
+    else
+        echo "Agent is not configured. Proceed to Configure." >> /tmp/MobSvc/sccm.log
+        Configure
+    fi
+else
+    Install
+fi
+
+cd /tmp
